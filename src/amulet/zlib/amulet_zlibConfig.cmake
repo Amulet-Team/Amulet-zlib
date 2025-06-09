643c39ca33cbf236c2835cea6c9b3e1a4bfe34c9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-09 16:26:18 +0100

    Fix cmake config

--- src/amulet/zlib/amulet_zlibConfig.cmake
+++ src/amulet/zlib/amulet_zlibConfig.cmake
@@ -5,9 +5,12 @@ if (NOT TARGET amulet_zlib)
     find_library(amulet_zlib_LIBRARY NAMES amulet_zlib PATHS "${CMAKE_CURRENT_LIST_DIR}")
     message(STATUS "amulet_zlib_LIBRARY: ${amulet_zlib_LIBRARY}")
 
-    add_library(amulet_zlib SHARED IMPORTED)
-    set_target_properties(amulet_zlib PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${amulet_zlib_INCLUDE_DIR}"
+    add_library(amulet_zlib_bin SHARED IMPORTED)
+    set_target_properties(amulet_zlib_bin PROPERTIES
         IMPORTED_IMPLIB "${amulet_zlib_LIBRARY}"
     )
+
+    add_library(amulet_zlib INTERFACE)
+    target_link_libraries(amulet_zlib INTERFACE amulet_zlib_bin)
+    target_include_directories(amulet_zlib INTERFACE ${amulet_zlib_INCLUDE_DIR})
 endif()
